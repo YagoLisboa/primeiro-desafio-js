bd4f8c7ce52666f967e49ebaef5e8933fdfd9cbf
YagoLisboa <yago.lisboa@live.com>
2023-12-28 02:44:35 -0300

    Melhorando o código da estrutura de decisão, pois não estava funcionando para todas as opções.

--- my first js project/index.js
+++ my first js project/index.js
@@ -5,28 +5,28 @@ var nivel;
 
 //Atribuindo valores para testes:
 nome = "Yago da Espada Sombria";
-xp = 2500;
+xp = 8500;
 
 //estrutura de decisão para apresentar as mensagens:
 if(xp<1000) {
     nivel="Ferro";
     console.log("O Herói de nome " + nome + " está no nível " + nivel + "!")
-} else if(1001<=xp<=2000){
+} else if(xp>=1001 && xp<=2000){
     nivel="Bronze";
     console.log("O Herói de nome " + nome + " está no nível " + nivel + "!")
-} else if(2001<=xp<=5000){
+} else if(xp>=2001 && xp<=5000){
     nivel="Prata";
     console.log("O Herói de nome " + nome + " está no nível " + nivel + "!")
-} else if(5001<=xp<=7000){
+} else if(xp>=5001 && xp<=7000){
     nivel="Ouro";
     console.log("O Herói de nome " + nome + " está no nível " + nivel + "!")
-} else if(7001<=xp<=8000){
+} else if(xp>=7001 && xp<=8000){
     nivel="Platina";
     console.log("O Herói de nome " + nome + " está no nível " + nivel + "!")
-} else if(8001<=xp<=9000){
+} else if(xp>=8001 && xp<=9000){
     nivel="Ascendente";
     console.log("O Herói de nome " + nome + " está no nível " + nivel + "!")
-} else if(9001<=xp<=10000){
+} else if(xp>=9001 && xp<=10000){
     nivel="Imortal";
     console.log("O Herói de nome " + nome + " está no nível " + nivel + "!")
 } else {
